3f1a0699e0203a359d4a28d38108df366f80ab49
AleOla30 <143103656+AleOla30@users.noreply.github.com>
2023-08-24 09:57:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 st.title('Frist App')
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciónes para interfaces multimodales")
-st.write("Facilmente puedo realizar backend t frontend")
+st.write("Facilmente puedo realizar backend y frontend")
 image = Image.open('Interfaces.png')
 
 st.image(image, caption='Interfaces multimodales')
