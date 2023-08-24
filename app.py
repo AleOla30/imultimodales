8b8c24c86af3690d2a3525be54be3a88e0c0efec
AleOla30 <143103656+AleOla30@users.noreply.github.com>
2023-08-24 09:51:38 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from PIL import Image
+
+st.title('Frist App')
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciónes para interfaces multimodales")
+st.write("Facilmente puedo realizar backend t frontend")
+image = Image.open('Interfaces.png')
+
+st.image(image, caption='Interfaces multimodales')
