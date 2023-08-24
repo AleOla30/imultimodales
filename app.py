b08b91c5dc28c57594db2bdcb983da44a71f4bea
AleOla30 <143103656+AleOla30@users.noreply.github.com>
2023-08-24 10:17:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,18 @@ st.image(image, caption='Interfaces multimodales')
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
 st.write('El texto escrito es', texto)
+
+st.subheader("Ahora usemos 2 Columnas")
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es mi primera columna")
+  st.write("Las interfaces multimodales mejoraran la experiencia del usuario")
+  resp = st.checkbox('Estoy de acuerdo')
+  if resp:
+    st.write('Correcto')
+
+with col2:
+  st.subheader("Esta es mi segunda columna")
+  modo = st.radio("Que modalidad es la principal en tu interfaz", ('visual','auditiva','tactil'))
